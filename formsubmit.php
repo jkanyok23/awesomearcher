99c779e42bd49df0d78717b6a1436d32da8ffc23
jkanyok23 <jkanyok23@gmail.com>
2017-06-06 20:37:56 -0400

    Added API
    Added hubspot API

--- formsubmit.php
+++ formsubmit.php
@@ -1,10 +1,51 @@
 <?php  
 
 $firstName = $_POST['firstName'];
-echo $firstName;
+// echo $firstName;
 $lastName = $_POST['lastName'];
 $username = $_POST['username'];
 // $password = $_POST['password'];
-$agreed = $_POST['agreed'];
+// $agreed = $_POST['agreed'];
+$phone = "1234567789";
+$company = "Awesome Archer";
+
+
+ //Process a new form submission in HubSpot in order to create a new Contact.
+
+$hubspotutk      = $_COOKIE['hubspotutk']; //grab the cookie from the visitors browser.
+$ip_addr         = $_SERVER['REMOTE_ADDR']; //IP address too.
+$hs_context      = array(
+    'hutk' => $hubspotutk,
+    'ipAddress' => $ip_addr,
+    'pageUrl' => 'http://www.example.com/form-page',
+    'pageName' => 'Example Title'
+);
+$hs_context_json = json_encode($hs_context);
+
+//Need to populate these variable with values from the form.
+$str_post = "firstname=" . urlencode($firstName) 
+    . "&lastname=" . urlencode($lastName) 
+    . "&email=" . urlencode($username) 
+    . "&phone=" . urlencode($phonenumber) 
+    . "&company=" . urlencode($company) 
+    . "&hs_context=" . urlencode($hs_context_json); //Leave this one be
+
+//replace the values in this URL with your portal ID and your form GUID
+$endpoint = 'https://forms.hubspot.com/uploads/form/v2/{portalId}/{formGuid}';
+
+$ch = @curl_init();
+@curl_setopt($ch, CURLOPT_POST, true);
+@curl_setopt($ch, CURLOPT_POSTFIELDS, $str_post);
+@curl_setopt($ch, CURLOPT_URL, $endpoint);
+@curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/x-www-form-urlencoded'
+));
+@curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response    = @curl_exec($ch); //Log the response from HubSpot as needed.
+$status_code = @curl_getinfo($ch, CURLINFO_HTTP_CODE); //Log the response status code
+@curl_close($ch);
+echo $status_code . " " . $response;
+
+
 
 ?>
