e7bd1879907c19fc85f7a0efe123328adbfdcdf4
jkanyok23 <jkanyok23@gmail.com>
2017-06-06 20:16:03 -0400

    Add echo / removed password
    I added an echo as a test to confirm the php is working.  I commented out the password requirement

--- formsubmit.php
+++ formsubmit.php
@@ -1,9 +1,10 @@
 <?php  
 
 $firstName = $_POST['firstName'];
+echo $firstName;
 $lastName = $_POST['lastName'];
 $username = $_POST['username'];
-$password = $_POST['password'];
+// $password = $_POST['password'];
 $agreed = $_POST['agreed'];
 
 ?>
